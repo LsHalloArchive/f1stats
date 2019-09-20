473bea606c1c6d3506f57fb7973d9befc737b23e
LsHallo <xlshallox@gmail.com>
2019-09-21 00:18:41 +0200

    added timeout to all connection in the main python script

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ subreddit_names = ["formula1", "formula1point5", "f1feederseries"]
 
 def send_to_mysql(host, username, password, database, users):
     mysql_db = MySQLdb.connect(host=host, user=username,
-                               passwd=password, db=database)
+                               passwd=password, db=database, connect_timeout=5)
     cursor = mysql_db.cursor()
     sql = 'INSERT INTO f1stats (time, f1, f1_5, f1feeder) VALUES (%s, %s, %s, %s)'
     val = (time.time(), users[subreddit_names[0]], users[subreddit_names[1]], users[subreddit_names[2]])
@@ -64,14 +64,16 @@ if __name__ == "__main__":
 
         # Insert into remote backup backup db
         try:
-            requests.post("https://f1status.000webhostapp.com/writeData.php", data={
-                'time': time.time(),
-                'f1': users[subreddit_names[0]],
-                'f1_5': users[subreddit_names[1]],
-                'f1feeder': users[subreddit_names[2]],
-                'token': config['mysql.000']['token'],
-                'uid': config['mysql.000']['uid']
-            })
+            requests.post("https://f1status.000webhostapp.com/writeData.php",
+                          data={
+                              'time': time.time(),
+                              'f1': users[subreddit_names[0]],
+                              'f1_5': users[subreddit_names[1]],
+                              'f1feeder': users[subreddit_names[2]],
+                              'token': config['mysql.000']['token'],
+                              'uid': config['mysql.000']['uid']
+                          },
+                          timeout=5)
         except Exception as exception:
             print(repr(exception))
 
